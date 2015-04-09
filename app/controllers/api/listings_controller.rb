162d4c8a4995df30045658682cf7408024f2133c
shibbi <shibo.fang@gmail.com>
2015-04-09 11:31:06 -0700

    Fix search bar not centering

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,6 +1,15 @@
 module Api
   class ListingsController < ApplicationController
     def search
+      @listings = Listing.find_by_location(search_params)
+      if @listing
+        render
+    end
+
+    private
+
+    def search_params
+      params.require(:search):permit(:location)
     end
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,9 @@ class Listing < ActiveRecord::Base
 
   validates :name, :description, :city, :state, :country, :price_daily,
             :price_weekly, :booked, :user_id, presence: true
+
+  def self.find_by_location(search_params)
+    location = search_params[:location]
+    city, state, country = location.split(',').map(&:strip)
+  end
 end
