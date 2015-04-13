6f4370918af25575ee9210c8f8d9f2243bb95901
shibbi <shibo.fang@gmail.com>
2015-04-13 15:22:29 -0700

    Move logic to translate coords into address into Backbone listing show view

--- app/assets/javascripts/views/listings/show.js
+++ app/assets/javascripts/views/listings/show.js
@@ -14,7 +14,25 @@ PetBnB.Views.ListingShowView = Backbone.View.extend({
     this.$el.html(content);
 
     PetBnB.setDatepickers();
+    this.getAddress();
 
     return this;
+  },
+
+  // find readable location based on lat and lng from listing data
+  getAddress: function () {
+    var lat = parseFloat(this.model.get('latitude'));
+    var lng = parseFloat(this.model.get('longitude'));
+    if (lat && lng) {
+      var latlng = new google.maps.LatLng(lat, lng);
+      var geocoder = geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+            $('.address').html(results[1].formatted_address);
+          }
+        }
+      });
+    }
   }
 });
